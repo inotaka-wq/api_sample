7af654c0c5bdea3f6742659305c6842bdda1b733
pino <tkinoue0324@gmail.com>
2023-10-27 22:04:58 +0900

    aaa

--- src/main/java/com/example/api_sample/ApiSampleApplication.java
+++ src/main/java/com/example/api_sample/ApiSampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ApiSampleApplication {
 
+	//
 	public static void main(String[] args) {
 		SpringApplication.run(ApiSampleApplication.class, args);
 	}
